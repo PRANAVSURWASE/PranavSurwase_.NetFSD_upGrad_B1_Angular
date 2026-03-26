2dd82abbfec4387d4e574fadffd38441d092d400
pranav surwase <pranavsurwase93@gmail.com>
2026-03-26 19:25:41 +0530

    ADDED ASP.NET CORE Assignments

--- ADO/Assignment_2/Program.cs
+++ ADO/Assignment_2/Program.cs
@@ -6,18 +6,19 @@
         {
             string connectionString = "Data Source=PRANAV-LAPTOP\\SQLEXPRESS;Initial Catalog=Student_db;Integrated Security=True;TrustServerCertificate=True;";
             EmployeeManager empManager = new EmployeeManager(connectionString);
+            /*
+                        empManager.InsertEmployee("Alice", 34000, "IT");
+                        empManager.InsertEmployee("Bob", 27560, "IT");
+                        empManager.InsertEmployee("AAA", 2309, "HR");
 
-            /* empManager.InsertEmployee("Alice", 34000, "IT");*/
-            //empManager.InsertEmployee("Bob", 27560, "IT");
-           // empManager.InsertEmployee("Vaibhav", 23000, "HR");
+                        Console.WriteLine("Employees in IT ");
+                        empManager.GetEmployeesByDepartment("HR");
 
-     /*       Console.WriteLine("Employees in IT ");
-            empManager.GetEmployeesByDepartment("HR");*/
-
-            Console.WriteLine("Updated Salary");
-            empManager.UpdateSalary(4,30000);
+                        Console.WriteLine("Updated Salary");
+                        empManager.UpdateSalary(4, 30000);
 
 
+                        */
             Console.WriteLine("Enter Employee ID  to Delete ");
             int empId = Convert.ToInt32(Console.ReadLine());
 
