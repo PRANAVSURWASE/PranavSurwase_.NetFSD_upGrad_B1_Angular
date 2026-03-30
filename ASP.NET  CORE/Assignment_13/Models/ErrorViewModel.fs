c022aa20a13671fbe8797559a19fd3efa021f8c5
pranav surwase <pranavsurwase93@gmail.com>
2026-03-30 19:20:28 +0530

    Removed duplicate Assignment_1 to Assignment_15 folders

--- ASP.NET  CORE/Assignment_13/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace Assignment_13.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
